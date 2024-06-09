27f7dac1d6481ca60d4a3196721cfd070520463e
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-06-09 11:27:26 -0300

    Ejercicio 3 Controladores

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getIndex(){
+        return view('category/index');
+    }
+
+    public function getShow($id){
+        return view('category/show',["id"=>$id]);
+    }
+
+    public function getCreate(){
+        return view('category/create');
+    }
+
+    public function getEdit($id){
+        return view('category/edit',["id"=>$id]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('/login', function () {
     return view('auth/login');
@@ -14,18 +14,10 @@ Route::get('/logout', function () {
     return view('auth/login');
 });
 
-Route::get('/category', function () {
-    return view('category/index');
-});
+Route::get('/category', [CategoryController::class, 'getIndex']);
 
-Route::get('/category/show/{id}', function ($id) {
-    return view('category/show', ['id' => $id]);
-});
+Route::get('/category/show/{id}', [CategoryController::class, 'getShow']);
 
-Route::get('/category/create', function () {
-    return view('category/create');
-});
+Route::get('/category/create', [CategoryController::class, 'getCreate']);
 
-Route::get('/category/edit/{id}', function ($id) {
-    return view('category/edit', ['id' => $id]);
-});
+Route::get('/category/edit/{id}', [CategoryController::class, 'getEdit']);

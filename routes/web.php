fd98b8e918f3f7425d1363749353eacb7d2fa218
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-06-09 08:09:58 -0300

    Ejercicio 1 Definición de Rutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
+});
+
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/category', function () {
+    return 'Listado de categorías';
+});
+
+Route::get('/category/show/{id}', function ($id) {
+    return "Vista detalle del post $id";
+});
+
+Route::get('/category/create', function () {
+    return 'Añadir post';
+});
+
+Route::get('/category/edit/{id}', function ($id) {
+    return "Modificar post $id";
 });

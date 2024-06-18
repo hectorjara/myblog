9738ed516318ab8b357fbf0d8c2782e9abf13d95
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-06-17 22:46:33 -0300

    Show Post

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,18 +1,32 @@
 @extends('layouts.layout')
 
-@section('content')
-<h1 class="p-2">Vista del Post {{$post->title}}</h1>
-<div class="p-2 flex min-w-0 gap-x-4">
-    <div class="min-w-0 flex-auto">
-        <p class="text-sm font-semibold leading-6 text-gray-900">{{$post->content}}</p>
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">
-        <p class="mt-1 truncate text-xs leading-5 text-gray-500">Creada por: {{$post->poster}}</p>
-    </div>
-  </div>
+@section('content')<!-- Post Section -->
+<section class="w-full md:w-2/3 flex flex-col items-center px-3">
 
-<a href="{{ url('/post/edit/' . $post->id) }}">
-    <button class="m-2 bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Editar
-    </button>
-</a>
+    <article class="flex flex-col shadow my-4">
+        <!-- Article Image -->
+        <a href="#" class="hover:opacity-75">
+            <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
+        </a>
+        <div class="bg-white flex flex-col justify-start p-6">
+            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">{{$post->category->nombre}}</a>
+            <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">{{$post->title}}</a>
+            <p href="#" class="text-sm pb-8">
+                Por <a href="#" class="font-semibold hover:text-gray-800">{{$post->poster}}</a>, Published on April 25th, 2020
+            </p>
+            <p>{{$post->content}}</p>
+        </div>
+    </article>
+    
+    <!-- Agregar Previous y Next -->
+
+    <div class="w-full flex flex-col text-center md:text-left md:flex-row shadow bg-white mt-10 mb-10 p-6">
+        <!-- Agregar Avatar del Autor -->
+        <div class="flex-1 flex flex-col justify-center md:justify-start">
+            <p class="font-semibold text-2xl">{{$post->poster}}</p>
+            <!-- Agregar Redes Sociales del Autor -->
+        </div>
+    </div>
+
+</section>
 @endsection

f81350f5013e5002a9bf14b5944af8bceacdfd99
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-06-17 23:20:07 -0300

    Agregar Post

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.layout')
 
 @section('content')
-<h1 class="p-2">Añadir Post</h1>
-
+<section class="w-full md:w-full flex flex-col  px-3">
+<h1 class="block text-lg text-gray-500 font-bold mb-1 pr-4">Añadir Post</h1>
 <form action="{{ url('post') }}" method="POST" class="p-2 ">
     @csrf
     <div class="p-2 flex-col">
@@ -42,11 +42,12 @@
         <label class="block text-gray-500 font-bold mb-1 pr-4">
           Contenido
         </label>
-        <textarea name="content" class="bg-gray-200 border-2 border-gray-200 rounded w-1000 h-500 py-2 px-4 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"></textarea>
+        <textarea name="content" class="bg-gray-200 border-2 border-gray-200 rounded  w-2/3  h-500 py-2 px-4 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"></textarea>
       </div>
 
       <button type="submit" class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Crear
       </button>
   </form>
+</section>
 @endsection
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,6 +4,10 @@
   <!-- Posts Section -->
   <section class="w-full md:w-2/3 flex flex-col items-center px-3">
 
+    <a href="{{ url('/post/create') }}" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+      Agregar Nuevo Post
+    </a>
+
     @foreach ($posts as $post)
       <article class="flex flex-col shadow my-4">
           <!-- Article Image -->

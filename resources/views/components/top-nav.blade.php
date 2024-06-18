6a8e93a6393f11b50b0a0a9ad136581938962919
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-06-18 05:31:59 -0300

    Vista Categorias solo para Logueados

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -5,7 +5,9 @@
             <nav>
                 <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
                     <li><a class="hover:text-gray-200 hover:underline px-4" href="{{ url('post') }}">Inicio</a></li>
+                    @auth                    
                     <li><a class="hover:text-gray-200 hover:underline px-4" href="{{ url('category') }}">Categorias</a></li>
+                    @endauth    
                 </ul>
             </nav>
 
